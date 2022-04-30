2f54348b456c6739b6879e0675809023824c66a8
Mirza Moulfi Ghozali <mirzaspc@gmail.com>
2022-05-01 06:36:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 apt-get update && apt-get install --no-install-recommends -y aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python2 python3 unzip zip openjdk-8-jdk
 apt-get autoremove -qqy && apt-get autoclean -qqy && rm -rf /var/lib/apt/lists/*
-curl -L https://github.com/AkihiroSuda/clone3-workaround/releases/download/v1.0.0/clone3-workaround.x86_64 -o noclone3 && chmod +x noclone3
+curl -L https://github.com/AkihiroSuda/clone3-workaround/releases/download/v1.0.0/clone3-workaround.x86_64 -o /noclone3 && chmod +x /noclone3
