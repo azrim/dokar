a7deb2745335d9bd48290de7f03ad6ea82a4cea6
Mirza Moulfi Ghozali <mirzaspc@gmail.com>
2022-06-14 10:07:03 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 TZ=Asia/Jakarta
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-apt-get update && apt-get install --no-install-recommends -y aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python2 python3 openssh-client unzip zip openjdk-8-jdk
+apt-get update && apt-get install --no-install-recommends -y aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python2 python3 openssh openssl unzip zip openjdk-8-jdk
 apt-get autoremove -qqy && apt-get autoclean -qqy && rm -rf /var/lib/apt/lists/*
